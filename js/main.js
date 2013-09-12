2be57d22ba0f681e3be1b48dfc8e1bfb7cb8eba6
Devesh Parekh <devesh@spamino.com>
2013-09-12 09:03:38 -0700

    License and cleanup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2013 Devesh Parekh All Rights Reserved.
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Injects the XHR wrapper into the page and sets up a relay between that page
+// and the background page, where all the communication to other social networks
+// occurs.
 function injectScript(file) {
     var s = document.createElement('script');
     s.src = chrome.extension.getURL(file);
